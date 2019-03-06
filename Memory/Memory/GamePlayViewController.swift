e0b45eacfd63311078460a2737303306eba809c8
Aaron London <github@postmechanical.com>
2019-03-06 11:11:29 -0700

    Implement game play

--- Memory/Memory/GamePlayViewController.swift
+++ Memory/Memory/GamePlayViewController.swift
@@ -12,6 +12,7 @@ class GamePlayViewController: UIViewController {
     @IBOutlet var gamePlayView: GamePlayView!
     
     var grid = Grid(width: 0, height: 0)
+    var previousSelectedCardView: CardView?
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -24,8 +25,16 @@ extension GamePlayViewController: GamePlayViewDelegate {
     func didSelect(_ cardView: CardView, at indexPath: IndexPath) {
         guard !cardView.isRevealed else { return }
         cardView.reveal()
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
-            cardView.reset()
+        guard let previousSelectedCardView = previousSelectedCardView else {
+            self.previousSelectedCardView = cardView
+            return
         }
+        if previousSelectedCardView.card.type != cardView.card.type {
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+                previousSelectedCardView.reset()
+                cardView.reset()
+            }
+        }
+        self.previousSelectedCardView = nil
     }
 }
