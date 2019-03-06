a7a959a2cdf2d160be228beb384a7a93e716caa9
Aaron London <github@postmechanical.com>
2019-03-06 11:35:07 -0700

    Add status label

--- Memory/Memory/GamePlayViewController.swift
+++ Memory/Memory/GamePlayViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class GamePlayViewController: UIViewController {
     @IBOutlet var gamePlayView: GamePlayView!
+    @IBOutlet var statusLabel: UILabel!
     
     var grid = Grid(width: 0, height: 0)
     var previousSelectedCardView: CardView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        statusLabel.text = "Let's play!"
+    }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -25,15 +31,19 @@ extension GamePlayViewController: GamePlayViewDelegate {
     func didSelect(_ cardView: CardView, at indexPath: IndexPath) {
         guard !cardView.isRevealed else { return }
         cardView.reveal()
+        statusLabel.text = "\(cardView.card.type.name)!"
         guard let previousSelectedCardView = previousSelectedCardView else {
             self.previousSelectedCardView = cardView
             return
         }
         if previousSelectedCardView.card.type != cardView.card.type {
+            statusLabel.text = "Try again!"
             DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
                 previousSelectedCardView.reset()
                 cardView.reset()
             }
+        } else {
+            statusLabel.text = "MATCH!"
         }
         self.previousSelectedCardView = nil
     }
